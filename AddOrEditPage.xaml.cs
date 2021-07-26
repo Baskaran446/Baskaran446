a3b055a2cc68cf0fc318ee070639d75111e25b19
Baskaran446 <87938557+Baskaran446@users.noreply.github.com>
2021-07-26 17:56:16 +0530

    AddorEditPage

---
+++ AddOrEditPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoListApp
+{
+    /// <summary>
+    /// Interaction logic for AddOrEditPage.xaml
+    /// </summary>
+    public partial class AddOrEditPage : Window
+    {
+        SubViewModel viewModel;
+        public AddOrEditPage()
+        {
+            InitializeComponent();
+            this.DataContextChanged += (o, e) =>
+            {
+                viewModel = (SubViewModel)(o as AddOrEditPage).DataContext;
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                viewModel.ButtonCommand.Execute("Back");
+            }), System.Windows.Threading.DispatcherPriority.Background);           
+            this.Close();
+        }
+    }
+}
